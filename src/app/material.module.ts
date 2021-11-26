0c685514cc86a5d3a2bad341d71f189d0acf739a
AgustinEscobarr <agustinescobaring@gmail.com>
2021-11-26 20:01:55 -0300

    Tabla de state

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -5,6 +5,7 @@ import {MatIconModule} from '@angular/material/icon';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
+import {MatTableModule} from '@angular/material/table';
 
 
 
@@ -22,7 +23,8 @@ import {MatInputModule} from '@angular/material/input';
     FormsModule,
     ReactiveFormsModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatTableModule
   ]
 })
 export class MaterialModule { }
